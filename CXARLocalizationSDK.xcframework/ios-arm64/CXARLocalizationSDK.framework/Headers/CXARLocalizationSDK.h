5d4acb3149e4e04cbfdfa7e1924be169ac0d536a
Muhammad Nauman Yousuf <nauman.yousuf@cxapp.com>
2024-05-29 18:19:51 +0000

    Auto-generated XCFrameworks for version 1.0.1

---
+++ CXARLocalizationSDK.xcframework/ios-arm64/CXARLocalizationSDK.framework/Headers/CXARLocalizationSDK.h
@@ -0,0 +1,18 @@
+//
+//  CXARLocalizationSDK.h
+//  CXARLocalizationSDK
+//
+//  Created by muhammad nauman on 4/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CXARLocalizationSDK.
+FOUNDATION_EXPORT double CXARLocalizationSDKVersionNumber;
+
+//! Project version string for CXARLocalizationSDK.
+FOUNDATION_EXPORT const unsigned char CXARLocalizationSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXARLocalizationSDK/PublicHeader.h>
+
+
