697743e9ac666b9257fddbe5b706238b5705a590
Zayan Tharani <zayantharani@folio3.com>
2024-10-03 12:19:46 +0500

    Update frameworks to target iOS 15.0

---
+++ DataSDK.xcframework/ios-arm64/DataSDK.framework/Headers/DataSDK.h
@@ -0,0 +1,18 @@
+//
+//  DataSDK.h
+//  DataSDK
+//
+//  Created by muhammad nauman on 5/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataSDK.
+FOUNDATION_EXPORT double DataSDKVersionNumber;
+
+//! Project version string for DataSDK.
+FOUNDATION_EXPORT const unsigned char DataSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSDK/PublicHeader.h>
+
+
